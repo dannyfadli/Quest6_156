36e8cf050b194b33b0245b7e68d03572114173da
dannyfadli <danyfdly95@gmail.com>
2025-11-21 10:15:24 +0700

    Tambahkan model datajenis kelamin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.lifectcle.runtime.compose)
+    implementation(libs.lifectcle.viewmodel.compose)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
---
+++ app/src/main/java/com/example/myarsitektur_mvvm/model/Data/DataJK.kt
@@ -0,0 +1,8 @@
+package com.example.myarsitektur_mvvm.model.Data
+
+object DataJK {
+    val Jenis =  listOf(
+        "Laki-Laki",
+        "Perempuan"
+    )
+}
