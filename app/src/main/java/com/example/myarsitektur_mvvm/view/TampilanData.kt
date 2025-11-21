fcfed2aabe704d431d56703bc2f3dc84af7cea47
dannyfadli <danyfdly95@gmail.com>
2025-11-21 09:59:53 +0700

    Tambahkan komponen isi ruang yg terdapat column

--- app/src/main/java/com/example/myarsitektur_mvvm/view/TampilanData.kt
+++ app/src/main/java/com/example/myarsitektur_mvvm/view/TampilanData.kt
@@ -34,25 +34,25 @@ fun TampilData(
         Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
         Pair(stringResource(id = R.string.jenis_kelamin),"Lainya"),
         Pair(stringResource(id = R.string.alamat),"Yogyakarta")
-//    )
-//    Scaffold (modifier = Modifier,
-//        {
-//            TopAppBar(
-//                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White)},
-//                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.teal_700))
-//            )
-//        }){ isiRuang->
-//        Column(modifier = Modifier.padding(isiRuang),
-//            verticalArrangement = Arrangement.SpaceBetween){
-//            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-//                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))){
-//                items.forEach { items->
-//                    Column {
-//                        Text(text = items.first.uppercase(), fontSize = 16.sp)
-//                        Text(text = items.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
-//                    }
-//                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
-//                }
+    )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.teal_700))
+            )
+        }){ isiRuang->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween){
+            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))){
+                items.forEach { items->
+                    Column {
+                        Text(text = items.first.uppercase(), fontSize = 16.sp)
+                        Text(text = items.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
 //                Spacer(modifier = Modifier.height(10.dp))
 //                Button(
 //                    modifier = Modifier.fillMaxWidth(),
